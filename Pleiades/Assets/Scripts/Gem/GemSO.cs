81f6fda94f5a3b96c20ad79ee9fc453c4a79e1f5
coderremarks <coderremarks@gmail.com>
2022-07-12 15:39:27 +0800

    J42

--- Pleiades/Assets/Scripts/Gem/GemSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObjects/Items/Gem")]
-public class GemSO : ItemSO
-{
-    public ElementalTypeSO elementalType;
-}
